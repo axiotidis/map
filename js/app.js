2541a312094f653b47a9cc9181cb9b1ba9695678
axiotidis <50782505+axiotidis@users.noreply.github.com>
2019-11-03 02:05:19 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -17,7 +17,8 @@ function setPosition(position) {
   var marker = new L.marker([lat, lng]).addTo(map);	//set a marker in current geoposition
   //var poptxt = "<b>You are here</b><br> Latitude  = "+lat.toString + " Longitude  =  " + lng.toString;
   //marker.bindPopup("<img src=\"img/pic.jpg\"/>");	//ok
-  marker.bindPopup("<br><br><b>You are here</b><br>");
+  //marker.bindPopup("<br><br><b>You are here</b><br>");	//ok
+  marker.bindPopup("<b>Latitude  =  </b>");
    //marker.bindPopup("<img src=\"img/pic.jpg\"/><br><br><b>You are here</b><br><b>Latitude  =  </b>+"lat.toString()"<br><b>Longitude  =  </b>+"lng.toString()"");
   //marker.bindPopup("<img src=\"img/pic.jpg\"/><br><br><b>You are here</b><br><b>Latitude  =  </b><br><b>Longitude  =  </b>");
   	   
